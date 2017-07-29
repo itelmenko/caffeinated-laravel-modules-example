59dcf255ee5426639c4d6c4bc4d6dc663f1c9dd4
Igor Telmenko <igor-telmenko@yandex.ru>
2017-07-29 16:41:10 +0300

    Package caffeinated/modules installed

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laravel modules
+        Caffeinated\Modules\ModulesServiceProvider::class,
+
     ],
 
     /*
@@ -225,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Module' => Caffeinated\Modules\Facades\Module::class,
     ],
 
 ];
